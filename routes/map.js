6e2765897cde1eeab9e0797fcfc80a14e4370612
Iva <iva95dobreva@gmail.com>
2019-04-16 22:47:28 +0900

    Now all pages work + Menu buttons change if logged in

--- routes/map.js
+++ routes/map.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const express = require('express');
+const cookies = require('cookie');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('map');
+  console.log(req.headers.cookie);
+  if(req.headers.cookie == undefined){
+    res.render('map', {token: "false"});
+  }
+
+  res.render('map', {token: "true"});
 });
 
 module.exports = router;
