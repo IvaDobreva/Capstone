b85c4d5c39bd4425e597403b8cf552a197b6158f
Iva <iva95dobreva@gmail.com>
2019-05-10 01:21:42 +0900

    router for index page updated removed obsolete code

--- routes/index.js
+++ routes/index.js
@@ -5,37 +5,26 @@ const router = express.Router();
 const cookies = require('cookie');
 
 const wrapper = require('../model/wrapper');
-const imageModel = require('../model/image');
-const vocModel = require('../model/vocabulary');
 
 router.get('/', wrapper.asyncMiddleware(async(req, res) => {
   //Check if logged in and redirect
   //if not logged in to login page
   //else to my page
 
-  const rows = await imageModel.getTotalRows();
-  const randImg = await imageModel.getRandImg(rows[0]['COUNT(*)']);
-  const kor = await vocModel.getTranslationKOR(randImg[0]['imgname']);
   console.log(req.headers.cookie);
 
   if(req.headers.cookie == undefined ) {
-    res.render("index", {image: "/images/"+randImg[0]['imgname'],
-    token: "false",
-    kor: kor[0]['kor_word']});
+    res.render("index", { token: "false"});
     res.end;
   }
-    if(cookies.parse(req.headers.cookie)['token'] == undefined ) {
-      res.render("index", {image: "/images/"+randImg[0]['imgname'],
-      token: "false",
-      kor: kor[0]['kor_word']});
-      res.end;
-    }
+  if(cookies.parse(req.headers.cookie)['token'] == undefined ) {
+    res.render("index", {token: "false"});
+    res.end;
+  }
 
   const token = cookies.parse(req.headers.cookie)['token'];
 
-  res.render("index", {image: "/images/"+randImg[0]['imgname'],
-                        token: "true",
-                        kor: kor[0]['kor_word']});
+  res.render("index", {token: "true"});
 
 }));
 
